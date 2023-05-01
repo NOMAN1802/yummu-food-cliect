7d7d1bfd45e092b384ab60384872920c3714a04b
NOMAN1802 <mustakimnoman82@gmail.com>
2023-05-01 23:18:15 +0600

    basic firebase rout bootstrap & other setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpJjV3j1F-CBY3psgDoMBFGVVR20EGWVE",
+  authDomain: "yummy-food-398a0.firebaseapp.com",
+  projectId: "yummy-food-398a0",
+  storageBucket: "yummy-food-398a0.appspot.com",
+  messagingSenderId: "118441261475",
+  appId: "1:118441261475:web:be44dc57140b073fd8c216"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
