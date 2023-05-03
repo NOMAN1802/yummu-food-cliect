2863ecf209cff317a55a7d5578ef697d4452607a
NOMAN1802 <mustakimnoman82@gmail.com>
2023-05-03 18:35:19 +0600

    registration validation

--- src/Login/Login/Login.jsx
+++ src/Login/Login/Login.jsx
@@ -1,37 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
 
 const Login = () => {
+
     const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     console.log('login page location', location)
-    const from = location.state?.from?.pathname || '/'
+    const from = location.state?.from?.pathname || '/';
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('')
 
     const handleLogin = event => {
         event.preventDefault();
+
+        setSuccess('');
+        setError('');
+
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
 
+        //  validation 
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one upper case');
+            return;
+        }
+        else if(!/(?=.*[0-9].*[0-9])/.test(password)){
+            setError('Please add at least 2 numbers');
+            return;
+        }
+        else if(password.length < 6 ){
+            setError('Please add at least 6 character in your password');
+            return;
+        }
+       
         signIn(email, password)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                navigate(from, { replace: true })
+                navigate(from, { replace: true });
+                setError('');
+                    event.target.reset();
+                    setSuccess('User has been login successfully');
             })
             .catch(error => {
-                console.log(error);
+                console.error(error.message);
+                setError('Email password does not matched');
             })
     }
 
     return (
         <Container className='w-25 mx-auto' style={{marginBottom: '150px',marginTop: '150px'}}>
-            <h3>Please Login</h3>
+            <h3 className='fw-bold'>Please <span className='text-primary'>Login</span></h3>
             <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
@@ -51,10 +77,10 @@ const Login = () => {
                     Don't Have an Account? <Link to="/register">Register</Link>
                 </Form.Text>
                 <Form.Text className="text-success">
-
+                <p>{success}</p>
                 </Form.Text>
                 <Form.Text className="text-danger">
-
+                <p>{error}</p>
                 </Form.Text>
             </Form>
         </Container>
--- src/Login/Register/Register.jsx
+++ src/Login/Register/Register.jsx
@@ -3,85 +3,110 @@ import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 import { useState } from 'react';
+import './Register.css'
 
 const Register = () => {
-   
-    const {createUser} = useContext(AuthContext);
-    const [accepted, setAccepted] = useState(false)
 
-   const handleRegister = event =>{
-    event.preventDefault();
-    const form = event.target;
-    const name = form.name.value
-    const photo = form.photo.value;
-    const email = form.email.value;
-    const password = form.password.value;
+    const { createUser } = useContext(AuthContext);
+    const [accepted, setAccepted] = useState(false);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('')
 
-    // console.log(name,photo,email,password);
+    const handleRegister = event => {
+        event.preventDefault();
+        setSuccess('');
+        setError('');
+        const form = event.target;
+        const name = form.name.value
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
 
-    createUser(email,password)
-    .then(result =>{
-        const createdUser = result.user;
-        // console.log(createdUser);
-    })
-    .catch(error =>{
-        console.log(error);
-    })
-   };
-   const handleAccepted =event=>{
-       setAccepted(event.target.checked);
-   }
+        // console.log(name,photo,email,password);
 
-    return (
-        <Container className='w-25 mx-auto'>
-            <h3>Please Register</h3>
-            <Form onSubmit={handleRegister}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" name='name' placeholder="Your Name" required />
-                    
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Photo URL </Form.Label>
-                    <Form.Control type="text" name='photo' placeholder="Photo Url" required />
-                    
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" required />
-                    
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name = 'password' placeholder="Password" required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check 
-                    onClick={handleAccepted}
-                    type="checkbox"
-                     name ='accept' 
-                     label={<>Accept <Link to ='/terms'>Terms and Conditions</Link></>} />
-                </Form.Group>
-                
-                <Button variant="primary" disabled = {!accepted} type="submit">
-                    Register
-                </Button>
-              <br />
-               <Form.Text className='text-secondary'>
-               Already Have An Account? 
-               <Link to ='/login'>Login</Link>
-               </Form.Text>
-               <Form.Text className='text-success'>
-                
-                </Form.Text>
-               <Form.Text className='text-danger'>
+        //  validation 
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one upper case');
+            return;
+        }
+        else if(!/(?=.*[0-9].*[0-9])/.test(password)){
+            setError('Please add at least 2 numbers');
+            return;
+        }
+        else if(password.length < 6 ){
+            setError('Please add at least 6 character in your password');
+            return;
+        }
 
-               </Form.Text>
-               
-            </Form>
-        </Container>
-    );
-};
+            createUser(email, password)
+                .then(result => {
+                    const createdUser = result.user;
+                    // console.log(createdUser);
+                    setError('');
+                    event.target.reset();
+                    setSuccess('User has been created successfully');
+                })
+                .catch(error => {
+                    console.error(error.message);
+                    setError(error.message);
 
-export default Register;
+                })
+        };
+        const handleAccepted = event => {
+            setAccepted(event.target.checked);
+        }
+
+        return (
+            <Container className='w-25 mx-auto reg'>
+                <h3 className='text-bolder'>Please <span className='text-primary'>Register</span></h3>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" name='name' placeholder="Your Name" required />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Photo URL </Form.Label>
+                        <Form.Control type="text" name='photo' placeholder="Photo Url" required />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name='email' placeholder="Enter email" required />
+
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name='password' placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check
+                            onClick={handleAccepted}
+                            type="checkbox"
+                            name='accept'
+                            label={<>Accept <Link to='/terms'>Terms and Conditions</Link></>} />
+                    </Form.Group>
+
+                    <Button variant="primary" disabled={!accepted} type="submit">
+                        Register
+                    </Button>
+                    <br />
+                    <Form.Text className='text-secondary'>
+                        Already Have An Account?
+                        <Link to='/login'>Login</Link>
+                    </Form.Text>
+                    <Form.Text className='text-success'>
+                        <p>{success}</p>
+                    </Form.Text>
+                    <Form.Text className='text-danger'>
+                        <p >{error}</p>
+                    </Form.Text>
+
+                </Form>
+            </Container>
+        );
+    };
+
+    export default Register;
